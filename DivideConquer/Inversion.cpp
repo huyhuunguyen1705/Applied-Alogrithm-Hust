60bdd8e748618f2a8dc3a209e13c6386839db983
Huu Huy Nguyen <huy.nh220028@sis.hust.edu.vn>
2024-11-17 15:07:33 +0700

    inversion

--- DivideConquer/Inversion.cpp
+++ DivideConquer/Inversion.cpp
@@ -13,3 +13,57 @@ Input
 Output
 6
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7, MAX = 1e6 + 5;
+int n, a[MAX];
+// the left and right array are allready sorted ascedingly
+long long mergeAndCount(int arr[] ,int left, int mid, int right){
+    int n1 = mid -left +1; //a[left] -> a[mid]
+    int n2 = right - mid; //a[mid+1] -> a[right]
+
+    int leftArr[n1], rightArr[n2];
+    for(int i=0; i<n1; i++) leftArr[i] = arr[left + i];
+    for(int i=0; i<n2; i++) rightArr[i] = arr[mid + 1 + i];
+
+    int i=0, j=0, k=left;
+
+    long long inversions = 0;
+    while(i<n1 && j<n2){
+        if(leftArr[i]<=rightArr[j]){
+            arr[k++] = leftArr[i++];
+        }else{
+            arr[k++] = rightArr[j++];
+            // All remaining elements in leftArr[i...n1-1] are greater than rightArr[j-1] since they are sorted ascedingly
+            inversions = (inversions + n1 - i) % MOD;
+        }
+    }
+    // Copy remaining elements of leftArr[] if any
+    while(i<n1) arr[k++] = leftArr[i++];
+    while(j<n2) arr[k++] = rightArr[j++];
+    return inversions;
+}
+
+long long mergeSortAndCount(int arr[],int left, int right){
+    long long inversions = 0;
+    if(left<right){
+        int mid = left + (right - left)/2;
+        inversions += mergeSortAndCount(arr, left, mid);
+        inversions %= MOD;
+        inversions += mergeSortAndCount(arr, mid+1, right);
+        inversions %= MOD;
+        inversions += mergeAndCount(arr, left, mid, right);
+        inversions %= MOD;
+    }
+    return inversions;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    for(int i=0; i<n; i++) cin >>a[i];
+    long long res = mergeSortAndCount(a, 0, n-1);
+    cout<<res<<'\n';
+}
